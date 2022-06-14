f602a87272bc464476d152ebfd27716c002b5a12
AminGanteng <89477551+AminGanteng@users.noreply.github.com>
2022-06-14 11:21:24 +0700

    Add files via upload

---
+++ plugins/general-menu2.js
@@ -0,0 +1,14 @@
+import moment from 'moment-timezone'
+let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
+let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
+let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
+conn.sendHydrated(m.chat, '*『 ɪɴᴛʀᴏᴅᴜᴄᴛɪᴏɴ 』*', `ꜱɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ ᴛᴏᴍʙᴏʟ ᴍᴇɴᴜ ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍᴘɪʟᴋᴀɴ ᴍᴇɴᴜ ʙᴏᴛ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://instagram.com/saya_asroriamin', 'Instagram', null, null, [
+[`ᴍᴇɴᴜ`, '.m'],
+[`\n*SAYA SUKA ASRORI AMIN*`, 'Bilek']
+], m, {asLocation: true})
+}
+handler.help = ['menu']
+handler.tags = ['general']
+handler.command = /^(menu)$/i
+
+export default handler
